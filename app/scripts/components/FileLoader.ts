2cdd6af6e6314f96d546eac05559c75d5712718f
Artem Brezhnev <brezzhnev@gmail.com>
2016-10-01 01:26:17 +0300

    FileLoader: implement simple uploaded files validation

--- app/scripts/components/FileLoader.ts
+++ app/scripts/components/FileLoader.ts
@@ -1,6 +1,3 @@
-// Allowed file types
-enum FileTypes { PNG, SVG }
-
 /**
  * Class FileLoader provide handling and preparing uploaded files for playground
  */
@@ -17,9 +14,25 @@ class FileLoader {
   files: FileList;
 
   /**
+   * Allowed file types
+   */
+  fileTypes: Array<String>;
+
+  /**
+   * Max file size in KB
+   */
+  _fileSize: number;
+
+  /**
    * @constructor
    */
   constructor() {
+
+    // Maximum uploaded file size
+    this.fileSize = 200;
+
+    // Allowed file types
+    this.fileTypes = ['jpg', 'jpeg', 'png'];
 
     this.input = <HTMLInputElement> document.getElementById('file-browse');
 
@@ -30,9 +43,68 @@ class FileLoader {
   /**
    * Handle open file process
    */
-  handleOpen() {
+  public handleOpen() {
 
     this.files = this.input.files;
+
+    // Verify uploaded files
+    this.validate();
+  }
+
+  /**
+   * Validate files
+   */
+  public validate() {
+
+    for(let i = 0; i < this.files.length; i++) {
+
+      if(!this._validateFile(this.files[i])) {
+
+        console.log('Validation failed');
+
+        return false;
+      }
+    }
+
+    console.log('Validate successful');
+
+    return true;
+  }
+
+  /**
+   * Validate file
+   * @param file
+   * @private
+   */
+  private _validateFile(file: File) {
+
+    // Check file size
+    if(file.size > this.fileSize) {
+
+      return false;
+    }
+
+    let fileType = /(?:\.([^.]+))?$/.exec(file.name)[1];
+
+    // Check file extension
+    return this.fileTypes.indexOf(fileType) !== -1;
+  }
+
+  /**
+   * Set max upload file size in KB
+   * @param size
+   */
+  set fileSize(size: number) {
+
+    this._fileSize = (size * 1024);
+  }
+
+  /**
+   * @returns {number}
+   */
+  get fileSize() {
+
+    return this._fileSize;
   }
 }
 
